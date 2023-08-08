de3d4d154a7865dcd7e4617c275aed527c3bca79
tutizaraz <dbarabashdev@gmail.com>
2023-08-08 15:53:30 +0200

    feat: Added doc for cleanVersion

--- src/cleanVersion.js
+++ src/cleanVersion.js
@@ -1,3 +1,16 @@
+// The cleanVersion function is designed to remove the caret (^) character from the version string. The caret is commonly used in package.json files to indicate that updates to dependencies should be automatically allowed as long as they do not include breaking changes (i.e., changes that increase the major version number in semantic versioning).
+
+// Let's break down the need for this function:
+
+// Semantic Versioning (SemVer): Dependency versions in JavaScript (and many other ecosystems) often follow SemVer, which has the format MAJOR.MINOR.PATCH, e.g., 1.4.2. Each of these components has a specific meaning:
+
+// MAJOR: Breaking changes.
+// MINOR: New features, but backward-compatible.
+// PATCH: Bug fixes and other minor changes, backward-compatible.
+// Caret (^) in Versioning: When you see a version like ^1.4.2 in a package.json file, it means "allow patches and minor updates, but no major updates". So, 1.4.3 or 1.5.0 would be acceptable updates, but 2.0.0 would not.
+
+// Reason for Removing the Caret: When working programmatically with versions (like when comparing them or fetching details from a registry), you often want to work with the exact version specified, not a range. Removing characters like ^ helps with this.
+
 function cleanVersion(version) {
   return version.replace("^", "");
 }
